05ae773df97da19435b6995b1f6c6d1d7f457fbd
Luciano Franca <luciano.franca@studenti.isissgobetti.it>
2023-03-27 13:17:48 +0200

    files codice

---
+++ CODE/negozio_di_alimentari_gruppo_3.js
@@ -0,0 +1,45 @@
+class Negozio {
+    constructor() {
+
+    }
+}
+class Cliente {
+    constructor() {
+        
+    }
+}
+class Prodotto {
+    constructor() {
+        
+    }
+}
+class ProdFresco  extends Prodotto {
+    constructor() {
+        
+    }
+}
+class ProdConfezionato extends Prodotto {
+    constructor() {
+        
+    }
+}
+class Confezione {
+    constructor() {
+        
+    }
+}
+class Lattina extends Confezione {
+    constructor() {
+        
+    }
+}
+class Barattolo extends Confezione {
+    constructor() {
+        
+    }
+}
+class Scatola extends Confezione {
+    constructor() {
+        
+    }
+}
